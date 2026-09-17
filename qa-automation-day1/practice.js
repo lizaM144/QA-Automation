78e02b581a88afa2873e6e11289ec7b018086f1f
Liza Maharjan <maharjanliza14@gmail.com>
2026-09-17 19:35:57 +0545

    feat(day1): javascript basic practice

---
+++ qa-automation-day1/practice.js
@@ -0,0 +1,34 @@
+// console.log("Name: Liza Maharjan");
+// console.log("Age: 25");
+// let add = "kathmandu";
+// console.log("Address: " + add);
+
+// let firstName = "Liza";
+// firstName = "Nisha";
+// console.log("First Name: " + firstName);
+
+// const pi = 3.14;
+// // pi = 3.14159; // This will throw an error because pi is a constant
+// console.log("Value of Pi: " + pi);
+
+// let x = 10;
+// console.log("Value of x: " + x);
+// x = 20;
+// console.log("Value of x: " + x);
+
+// var y = 30;
+// console.log("Value of y: " + y);
+// y = 40;
+// console.log("Value of y: " + y);
+
+console.log(5 =="5") //value comparison with type coercion
+console.log(5 ==="5") // value comparison without type coercion
+
+age = 20;
+if(!(age <= 18)) // the condition is true because age is greater than 18, so the code inside the if block will execute
+{
+    console.log("Age is not valid");
+}
+
+let Name = "Liza";
+console.log(`First Name: ${Name}`) //template literal usage
