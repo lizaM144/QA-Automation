6ce66c00cfb18f504a10b4fd1126779cfcf029a6
Liza Maharjan <maharjanliza14@gmail.com>
2026-09-21 14:48:05 +0545

    feat(day4): add function to convert celcius to fahrenheit

---
+++ qa-automation-day4/assignment_part1/q10_cToF.js
@@ -0,0 +1,5 @@
+// Q10 
+// Task: Write an arrow function cToF that converts Celsius to Fahrenheit using the formula (C × 9/5) + 32. Return the result
+const cToF = (c) => (c *9/5) + 32; // since this function has only one expression, we don't have to write return
+console.log(cToF(0));
+console.log(cToF(100));
