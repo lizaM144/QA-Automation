00895683e05c12b5888c24bf76820c273fb92723
Liza Maharjan <maharjanliza14@gmail.com>
2026-09-22 12:37:58 +0545

    refactor: updated comment

--- qa-automation-day4/assignment_part2/q13_swap.js
+++ qa-automation-day4/assignment_part2/q13_swap.js
@@ -1,7 +1,7 @@
 // Q13
 // Task: Write swap(a, b) that returns [b, a] using array destructuring — without a temporary variable
 function swap(a,b) {
-    [a,b] = [b,a]; // destructuring
+    [a,b] = [b,a]; // destructuring array
     return [a,b];
 }
 console.log(swap(1,2)); // [ 2, 1]
